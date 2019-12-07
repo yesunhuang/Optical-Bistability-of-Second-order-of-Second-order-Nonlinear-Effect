a2d9c8d84823a0920f992fd1cb13f92e139583a3
YesunHuang <tgbus944812203@hotmail.com>
2019-12-07 23:59:48 +0800

    generate data

--- Optical Bistability of Second-order of Second-order Nonlinear Effect/ProblemMain.py
+++ Optical Bistability of Second-order of Second-order Nonlinear Effect/ProblemMain.py
@@ -71,8 +71,9 @@ class OBF:
                 axes[0].plot(E*E,self.Result_out_m[j,...,0],color=cor,linestyle='--',label='M:g='+str(round(g[j],2)))
                 axes[1].plot(E*E,self.Result_out_m[j,...,1],color=cor,linestyle='--',label='M:g='+str(round(g[j],2)))
         else:
-            axes[0].set_xlim(g[0]*g[0],g[-1]*g[-1]);axes[1].set_xlim(g[0]*g[0],g[-1]*g[-1]);
+            axes[0].set_xlim(g[0],g[-1]);axes[1].set_xlim(g[0],g[-1]);
             for j in range(0,np.size(E)):
+                cor=self.randomcolor()
                 axes[0].plot(g,self.Result_out[...,j,0],color=cor,linestyle='-',label='Q:'+r"$E^2= $"+str(round(E[0]*E[0],2)))
                 axes[1].plot(g,self.Result_out[...,j,1],color=cor,linestyle='-',label='Q:'+ r"$E^2= $"+str(round(E[0]*E[0],2)))
                 axes[0].plot(g,self.Result_out_m[...,j,0],color=cor,linestyle='--',label='M:'+r"$E^2= $"+str(round(E[0]*E[0],2)))
